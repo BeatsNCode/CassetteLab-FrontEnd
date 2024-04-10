3ca0e8ce4b7405a52195c3040ebce8291aa9df49
Jean Augustin <jre.augustin@gmail.com>
2024-04-09 22:50:39 -0400

    Create Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-// import { useState } from 'react'
 import theme from './theme'
 import { ThemeProvider,  } from '@mui/material/styles';
-import './App.css'
-
+import AppBar from './components/shared/NavBar.tsx';
+import { CssBaseline } from '@mui/material';
 
 function App() { 
 
   return (
     <>
     <ThemeProvider theme={theme}>
-      <h1>CassetteLab</h1>
-
-
+      <AppBar></AppBar>
+      <CssBaseline />
     </ThemeProvider>
       
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-import theme from './theme';
-import { CssBaseline } from '@mui/material';
+// import theme from './theme';
+// import { CssBaseline } from '@mui/material';
 
-import { ThemeProvider } from '@emotion/react';
+// import { ThemeProvider } from '@emotion/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
+    {/* <ThemeProvider theme={theme}> */}
       <App />
-    </ThemeProvider>
+    {/* </ThemeProvider> */}
   </React.StrictMode>,
 );
 
