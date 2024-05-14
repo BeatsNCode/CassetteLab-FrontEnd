dbef2d66c2c4125c8d848b23686a3b7bd57df903
Jean Augustin <jre.augustin@gmail.com>
2024-05-14 13:43:19 -0400

    Remove items from localStorage on signout

--- src/components/UI/Auth/signOut.tsx
+++ src/components/UI/Auth/signOut.tsx
@@ -6,5 +6,9 @@ export default function signOut() {
         method: "post",
         url: "http://127.0.0.1:8000/dj-rest-auth/logout/"
       })
+      .then(() => {
+        localStorage.removeItem("CLU");
+        localStorage.removeItem("CLabLogin");
+      })
     );
 }
