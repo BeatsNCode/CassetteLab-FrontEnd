4b245e2bad7d41169f9a600937a11ac0d78d5a2e
Jean Augustin <jre.augustin@gmail.com>
2024-05-25 17:12:35 -0400

    begin configuring profile page

--- src/components/UI/Pages/artistProfilePage.tsx
+++ src/components/UI/Pages/artistProfilePage.tsx
@@ -1,5 +1,107 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import Grid from '@mui/material/Grid';
+import { UserContext } from '../../../Contexts/userContext';
+import { axiosInstance } from '../../../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
+
+// Create function to fetch artist details
+
+// Create function to update artist details
+
+// Create function to change password
+
+// Create function to delete account
+
 export default function ArtistProfilePage() {
+    const userContext = React.useContext(UserContext);
+    const loggedInUser = userContext.user;
+    const navigate = useNavigate();
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const stageName = data.get('stageName')
+        const location = data.get('location')
+        
+
+        
+      };
+
+
     return (
-        <h1>Profile Page</h1>
+        <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+          marginTop: 10 ,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+        >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <ManageAccountsIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Your Profile
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+            <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="stageName"
+                label="Your Band/Stage name"
+                name="stageName"
+                autoComplete="Band/Stage name"
+                autoFocus
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+                required
+                margin="normal"
+                fullWidth
+                id="location"
+                label="Where you're based"
+                name="location"
+                autoComplete="location"
+                autoFocus
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ paddingBottom: 2}}>
+            <TextField
+                    required
+                    margin="normal"
+                    fullWidth
+                    id="genres"
+                    label="Your genre(s)"
+                    name="genres"
+                    autoComplete="genres"
+                    autoFocus
+                />
+          </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            >
+            Save Changes
+          </Button>
+            
+        </Box>
+    </Box>
+    </Container>  
     )
 }
