62961cc80c419a8b0a2de6d65543e2fbb4b1590a
Jean Augustin <jre.augustin@gmail.com>
2024-06-05 20:52:20 -0400

    Enable /user fetching in Settings page - log results in console for now

--- src/components/UI/Pages/accountSettings/accountSettingsPage.tsx
+++ src/components/UI/Pages/accountSettings/accountSettingsPage.tsx
@@ -1,8 +1,28 @@
-// Build function to get user details
-// Endpoint: /dj-rest-auth/user/
+import * as React from 'react';
+import { axiosInstance } from '../../../../utils/axiosInstance';
+import { UserContext } from '../../../../Contexts/userContext';
+
+async function fetchUser(token: any) {
+    try {
+        const response = await axiosInstance.get(`/dj-rest-auth/user/`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user details:', error);
+        throw error;
+    }
+}
 
 
 export default function AccountSettingsPage() {
+    const userContext = React.useContext(UserContext);
+    const loggedInUser = userContext.user;
+    fetchUser(loggedInUser.CLToken)
+    .then((response) => console.log(response))
+
     return (
 
         // Build password change form
